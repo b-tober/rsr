352ec7accfaaf6c5933dd0bbc5e5f8d502436238
brandon <btober@arizona.edu>
2019-05-16 16:03:16 -0700

    cleanup

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main(file_name,winsize = 1000, sampling = 250, nbcores = 2):
     # Apply RSR along a vector of successive amplitude.
     # The RSR is applied on windows made of (winsize) values. Each window is separated by
     # (sampling) samples (can be time consuming).
-    w,a = rsr.run.along(amp, winsize=winsize, sampling=sampling, nbcores=2,verbose=False)
+    w,a = rsr.run.along(amp, winsize=winsize, sampling=sampling, nbcores=2,verbose=True)
 
     # rsr.utils.plot_along(a) # Plot results
     # plt.show()
@@ -41,7 +41,7 @@ def main(file_name,winsize = 1000, sampling = 250, nbcores = 2):
 
     try:
         np.savetxt(out_path + file_name.split('_')[0] + '_' + file_name.split('_')[1] + '_rsr.csv', 
-        out, delimiter = ',', newline = '\n', header = header, fmt = '%s') 
+        out, delimiter = ',', newline = '\n', comments = '', header = header,  fmt = '%s') 
     except Exception as err:
         print(err)
 
@@ -54,10 +54,11 @@ if __name__ == '__main__':
     # ---------------
     # INPUTS - set to desired parameters
     # ---------------
-    study_area = 'edr_test/'  
+    study_area = 'bh_nh_bt/'  
     winsize = 1000              # window size for fit
     sampling = 250              # step size for fit along track
     nbcores = 2
+    verbose = False             # report results of fit if true
     # ---------------
     mars_path = '/MARS'
     in_path = mars_path + '/targ/xtra/SHARAD/EDR/surfPow/' + study_area
@@ -67,7 +68,7 @@ if __name__ == '__main__':
         mars_path = '/media/anomalocaris/Swaps' + mars_path
         in_path = '/media/anomalocaris/Swaps' + in_path
         out_path = '/media/anomalocaris/Swaps' + out_path
-    elif os.getcwd().split('/')[1] == 'mnt':
+    elif os.getcwd().split('/')[1] == 'mnt':False
         mars_path = '/mnt/d' + mars_path
         in_path = '/mnt/d' + in_path
         out_path = '/mnt/d' + out_path
@@ -86,4 +87,4 @@ if __name__ == '__main__':
         pass
 
     file_name = sys.argv[1]     # input geom file with surface reflectivity for each trace
-    main(file_name, winsize = winsize, sampling = sampling, nbcores = nbcores)
+    main(file_name, winsize=winsize, sampling=sampling, nbcores=nbcores, verbose=verbose)
